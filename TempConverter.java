ada9c2cafd2390c40b4aae5d4f46235c3af31131
Juarez-knudsen <S02944712@ccc.ccofc.edu>
2024-01-31 18:05:28 -0700

    Added temperature converter

--- TempConverter.java
+++ TempConverter.java
@@ -1,3 +1,25 @@
+// Temperature Converter from Fahrenheit to Celsius then to Kelvin
+// Evan Knudsen
+
+import java.util.Scanner;
+
 public class TempConverter {
-    // Constants
+    public static void main(String[] args) {
+        // CONSTANTS
+        final double FREEZING_POINT_WATER_IN_F = 32;
+        final double MELTING_POINT_ICE_IN_K = 273.15;
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Please enter temperature in Fahrenheit: ");
+        double fahrenheit = in.nextDouble();
+
+        double celsius = ((fahrenheit * 5) / 9) + FREEZING_POINT_WATER_IN_F;
+
+        double kelvin = celsius + MELTING_POINT_ICE_IN_K;
+
+        System.out.println(fahrenheit + " degrees Fahrenheit is equal to " + celsius + " degrees Celsius.");
+
+        System.out.println(celsius + " degrees celsius is equal to " + kelvin + " degrees Kelvin");
+    }
 }
